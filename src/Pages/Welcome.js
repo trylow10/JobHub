4e87c61b5754f8d37899d5fc5474b0e72ee07c8d
Trilochan Aryal <aryaltrylowchan@gmail.com>
2023-05-29 00:11:51 +0545

    added: missing welcome page

--- src/Pages/Welcome.js
+++ src/Pages/Welcome.js
@@ -1,1 +1,28 @@
+// Libraries
+import React from "react";
 
+// Functional Components
+import Header from "../Components/Welcome/Header";
+import Feature from "../Components/Welcome/Feature"
+import MainImg from "../Components/Welcome/MainImg"
+import OpenSection from "../Components/Welcome/OptionSection"
+import Review from "../Components/Welcome/OptionSection"
+import Video from "../Components/Welcome/OptionSection"
+// Styled Components
+
+
+const Welcome = ()=>{
+  return (
+    <div>
+     <Header></Header>
+    <MainImg></MainImg>
+     <Feature></Feature>
+    {/* <OpenSection></OpenSection> */}
+    {/* <Review></Review>
+    <Video></Video> */}
+
+    </div>
+  );
+};
+
+export default Welcome;
